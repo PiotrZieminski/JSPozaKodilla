7119f36a0f118cc352c1ba2ab1d6d039b09aabb9
Michael Gacek <gacek.michael@outlook.com>
2017-02-06 18:01:28 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -11,15 +11,7 @@ require.config({
     waitSeconds: 15 
 });
 
-requirejs(['movie'], function(Movie) {
-    var movie1 = new Movie(),
-        movie2 = new Movie("Test", 3, 2005, true);
-
-    console.log(movie2);
-
-});
-
-requirejs(['library'], function(Library) {
+requirejs(['movie', 'library'], function(Movie, Library) {
     var movie1 = new Movie(),
         movie2 = new Movie("Test", 3, 2005, true);
 
